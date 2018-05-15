095de8aac0923b3b598fefb07aff655530016b9b
Tobias Fuchs <tobias.fuchs@tum.de>
2018-05-15 15:36:29 +0200

    initial commit

---
+++ functions.sh
@@ -0,0 +1,22 @@
+# This file contains some useful function definitions
+
+# make and change in new directory
+mkcd () {
+    mkdir -p "$*"
+    cd "$*"
+}
+
+function get_pwd(){
+  git_root=$PWD
+  while [[ $git_root != / && ! -e $git_root/.git ]]; do
+    git_root=$git_root:h
+  done
+  if [[ $git_root = / ]]; then
+    unset git_root
+    prompt_short_dir=%~
+  else
+    parent=${git_root%\/*}
+    prompt_short_dir=${PWD#$parent/}
+  fi
+  echo $prompt_short_dir
+}
